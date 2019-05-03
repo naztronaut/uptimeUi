0d16796cc7254aa3ec69a05cb0e32ad86fad7c35
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-05-03 00:10:32 -0400

    dashboard led component set

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {LedService} from '../services/led.service';
+import {Led} from '../model/led';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.less']
+  styleUrls: ['./dashboard.component.less'],
+  providers: [LedService]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  leds: Led[];
+  constructor(private ledService: LedService) { }
 
   ngOnInit() {
+    this.getLeds();
+  }
+
+  getLeds(): void {
+    this.ledService.getLeds().subscribe(res => {
+      this.leds = res.slice().reverse();
+    });
   }
 
 }
