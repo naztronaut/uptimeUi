418d380d698ed5bb758c6d70bbe5a648360578fc
Nazmus Nasir <nazmus@hbs.edu>
2019-05-06 23:39:36 -0400

    tooltip module to dashboard.component.spec.ts

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {MatSnackBarModule, MatTableModule} from '@angular/material';
+import {MatSnackBarModule, MatTableModule, MatTooltipModule} from '@angular/material';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {ChartsModule} from 'ng2-charts';
 import {MinutesSincePipe} from '../minutes-since.pipe';
@@ -15,7 +15,7 @@ describe('DashboardComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DashboardComponent, MinutesSincePipe ],
-      imports: [FormsModule, MatTableModule, HttpClientTestingModule, MatSnackBarModule, ChartsModule],
+      imports: [FormsModule, MatTableModule, HttpClientTestingModule, MatSnackBarModule, ChartsModule, MatTooltipModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
